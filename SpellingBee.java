a0356586eb37645284a996ad505660ea28dca9fa
Jacob Archambault <jacobarchambault@localhost.localdomain>
2020-02-12 21:32:35 -0500

    First commit

---
+++ SpellingBee.java
@@ -0,0 +1,30 @@
+/*
+ * SpellingBee.java
+ * 
+ * Copyright 2020 Jacob Archambault <jacobarchambault@e0-ca-94-9e-d5-99>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+import static java.lang.System.*;
+
+public class SpellingBee {
+	public static void main (String[] args) {
+		console().printf("Welcome to the spelling bee!");
+	}
+}
